b31141b5cbb3d447050afe561b71886958ec3949
Maximilian Fellner <max.fellner@gmail.com>
2015-03-15 19:04:55 +0100

    Use ES6 classes for components

--- static/jsx/body.jsx
+++ static/jsx/body.jsx
@@ -4,29 +4,15 @@ const NavBar = require('./nav-bar.jsx');
 /**
  * Component: Body
  */
-const Body = React.createClass({
+class Body extends React.Component {
 
-  propTypes: {
-    navItems: React.PropTypes.array.isRequired,
-    scripts: React.PropTypes.array,
-    styles: React.PropTypes.array
-  },
-
-  getDefaultProps: function () {
-    return {
-      navItems: [],
-      scripts: [],
-      styles: []
-    };
-  },
-
-  render: function () {
+  render() {
     /* jshint ignore:start */
     return (
       <html lang="en">
         <meta charSet="utf-8"/>
         <head>
-        {this.props.styles.map(function (style, i) {
+        {this.props.styles.map((style, i) => {
           return (
             <link key={i} rel="stylesheet" href={style}/>
           );
@@ -34,7 +20,7 @@ const Body = React.createClass({
         </head>
         <body className="container">
           <div className="row">
-            <NavBar className="col-md-2" items={this.props.navItems} selected={0}/>
+            <NavBar className="col-md-2" items={this.props.navItems}/>
             <div className="col-md-10">
               <h3>Hello, Body component</h3>
               <p>Lorem ipsum, dolor sit amet.</p>
@@ -44,7 +30,7 @@ const Body = React.createClass({
             __html: 'var APP_PROPS = ' + JSON.stringify(this.props) + ';'
           }}>
           </script>
-        {this.props.scripts.map(function (script, i) {
+        {this.props.scripts.map((script, i) => {
           return (
             <script key={i} src={script}/>
           );
@@ -54,6 +40,18 @@ const Body = React.createClass({
     );
     /* jshint ignore:end */
   }
-});
+}
+
+Body.propTypes = {
+  navItems: React.PropTypes.array.isRequired,
+  scripts : React.PropTypes.array,
+  styles  : React.PropTypes.array
+};
+
+Body.defaultProps = {
+  navItems: [],
+  scripts : [],
+  styles  : []
+};
 
 module.exports = Body;
--- static/jsx/nav-bar.jsx
+++ static/jsx/nav-bar.jsx
@@ -3,44 +3,23 @@ const React = require('react');
 /**
  * Component: NavBar
  */
-const NavBar = React.createClass({
+class NavBar extends React.Component {
 
-  propTypes: {
-    className: React.PropTypes.string,
-    items: React.PropTypes.array.isRequired,
-    selected: function (props, propName, componentName) {
-      const max = props.items.length - 1;
-      const prop = props[propName];
-      if (typeof prop !== 'number' ||
-        prop < 0 ||
-        prop > max) {
-        return new Error(`Invalid prop '${propName}' of value '${prop}' ` +
-        `supplied to '${componentName}', must be number between 0 and ${max}.`);
-      }
-    }
-  },
+  constructor(props) {
+    super(props);
+    this.state = {selected: this.props.selected};
+  }
 
-  getDefaultProps: function () {
-    return {
-      items: [],
-      selected: 0
-    };
-  },
-
-  getInitialState: function () {
-    return {selected: this.props.selected};
-  },
-
-  handleClick: function (index) {
+  handleClick(index) {
     this.setState({selected: index});
-  },
+  }
 
-  render: function () {
+  render() {
     /* jshint ignore:start */
     return (
       <div className={this.props.className}>
         <ul className="nav nav-pills nav-stacked">
-         {this.props.items.map(function (item, i) {
+         {this.props.items.map((item, i) => {
            const boundClick = this.handleClick.bind(this, i);
            return (
              <li key={i} className={this.state.selected === i ? 'active' : ''}>
@@ -53,6 +32,26 @@ const NavBar = React.createClass({
     );
     /* jshint ignore:end */
   }
-});
+}
+
+NavBar.propTypes = {
+  className: React.PropTypes.string,
+  items: React.PropTypes.array.isRequired,
+  selected: (props, propName, componentName) => {
+    const max = props.items.length - 1;
+    const prop = props[propName];
+    if (typeof prop !== 'number' ||
+      prop < 0 ||
+      prop > max) {
+      return new Error(`Invalid prop '${propName}' of value '${prop}' ` +
+      `supplied to '${componentName}', must be number between 0 and ${max}.`);
+    }
+  }
+};
+
+NavBar.defaultProps = {
+  items: [],
+  selected: 0
+};
 
 module.exports = NavBar;
